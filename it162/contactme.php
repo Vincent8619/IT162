3d982c4e5382433dfdcb254ac6a2e2f74008ca9a
Vincent8619 <56529046+Vincent8619@users.noreply.github.com>
2021-01-24 16:23:06 -0800

    Add files via upload
    updated with header/template/footer.php files

---
+++ it162/contactme.php
@@ -0,0 +1,8 @@
+<?php include "includes/header.php";?>
+
+<!-- MAKE SURE YOU GET YOUR (3) IMAGES SAVED INTO YOUR IMAGES FOLDER -->
+<h1 class="pageID">Contactme</h1>
+
+
+
+<?php include "includes/footer.php";?>
---
+++ it162/index.php
@@ -0,0 +1,19 @@
+<!-- see Bill's Config file training video to learn how to add and use a config.php file -->
+<?php include "includes/header.php";?>
+
+<!-- MAKE SURE YOU GET YOUR (3) IMAGES SAVED INTO YOUR IMAGES FOLDER -->
+<h1 class="pageID">Welcome</h1>
+
+ <img src="images/phone.jpg" class="phone" alt="Rainbow flag Space Needle"/>
+ <p>My name is Steve Taylor and I'm currently in the process of becoming a Web Developer. I specifically have a strong interest in front end developement and enjoy the challenge of creating something new. I am originally from Arizona, but have called Seattle home now since 2014. I live with my partner Jared and our two dogs, Gizmo and Otis. My interests include: video games, board games, hiking, movies, and reading when I have the time!</p>
+</section>
+<!-- END LEFT COL -->
+
+<!-- START RIGHT COL -->
+<aside>
+ <img src="images/tablet.jpg" class="tablet" alt="Steve"/>
+ <img src="images/desktop.jpg" class="desktop" alt="Steve and Jared"/>
+</aside>
+<!-- END RIGHT COL -->
+
+<?php include "includes/footer.php";?>
